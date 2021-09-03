2ba3d25c3db4b1f3f3aaf75744b58323b55eed56
cbakcoleman <88118758+cbakcoleman@users.noreply.github.com>
2021-09-02 17:10:24 -0700

    test if for loop works for color changes

--- server.py
+++ server.py
@@ -5,5 +5,11 @@ app = Flask(__name__)
 def eight_by():
     return render_template("index.html")
 
+@app.route('/<int:num1>/<int:num2>/<color1>/<color2>')
+def sensei(num1, num2, color1, color2):
+    return render_template("index.html", 
+    num1 = num1, num2 = num2, 
+    color1 = color1, color2 = color2)
+
 if __name__=="__main__":   # Ensure this file is being run directly and not from a different module    
     app.run(debug=True)    # Run the app in debug mode.
