af4ba4b3e355463d767ea9b895e75dadb6eb3010
cbakcoleman <88118758+cbakcoleman@users.noreply.github.com>
2021-09-02 19:25:06 -0700

    finalize app routes

--- server.py
+++ server.py
@@ -2,8 +2,22 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
-def eight_by():
-    return render_template("index.html")
+def checkerbaord():
+    return render_template("index.html",
+    num1 = 8, num2 = 8,
+    color1 = "papayawhip", color2 = "cadetblue")
+
+@app.route('/<int:num1>')
+def single(num1):
+    return render_template("index.html",
+    num1 = num1, num2 = 8,
+    color1 = "papayawhip", color2 = "cadetblue")
+
+@app.route('/<int:num1>/<int:num2>')
+def ninja(num1, num2):
+    return render_template("index.html",
+    num1 = num1, num2 = num2,
+    color1 = "papayawhip", color2 = "cadetblue")
 
 @app.route('/<int:num1>/<int:num2>/<color1>/<color2>')
 def sensei(num1, num2, color1, color2):
